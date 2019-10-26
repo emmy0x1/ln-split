c74899ec765da498d390351bec3c0904768fe417
emmy <almaraz.emily@gmail.com>
2019-10-26 10:40:54 -0500

    Set up webpack config.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,20 +1,40 @@
-var config = {
-  entry: './index.js',
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DotenvPlugin = require("dotenv-webpack");
+const webpack = require("webpack");
+const path = require("path");
 
-  output: {
-    path: 'build/',
-    filename: 'index.js',
-  },
+const clientDir = path.resolve(__dirname);
 
+module.exports = {
+  mode: "development",
+  entry: path.join(clientDir, "index.js"),
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loaders: ["babel-loader"],
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
       }
     ]
+  },
+  plugins: [
+    // Adds our .env variables to the build
+    new DotenvPlugin({ path: "../.env", systemvars: true }),
+    // Takes our index.html template, and injects our build into it
+    new HtmlWebpackPlugin({
+      template: `${clientDir}/index.html`,
+      inject: true
+    })
+  ],
+  resolve: {
+    modules: [clientDir, path.join(__dirname, "../node_modules")]
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+    stats: "minimal"
   }
-}
-
-module.exports = config;
+};
