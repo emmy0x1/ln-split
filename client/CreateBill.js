03417c911efa78a5d6cfafb420868454fa023471
Anthony Ronning <anthonyronning@gmail.com>
2019-11-14 10:54:49 +0100

    Adding userId to create bill

--- client/CreateBill.js
+++ client/CreateBill.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import api from "./lib/api";
+import userContext from "./context/userContext";
 
 const CreateBill = () => {
   const [descriptionState, setDescriptionState] = useState("");
@@ -7,6 +8,7 @@ const CreateBill = () => {
 
   const blankUser = { name: "", amount: "" };
   const [userState, setUserState] = useState([{ ...blankUser }]);
+  const [userIdState, setUserIdState] = useState("");
 
   const handleBillChange = e => {
     if (e.target.name === "description") {
@@ -32,73 +34,81 @@ const CreateBill = () => {
 
   const handleSubmit = evt => {
     evt.preventDefault();
-    api.createBill(descriptionState, totalAmountState, {
+    api.createBill(userIdState, descriptionState, totalAmountState, {
       userAmounts: userState
     });
   };
 
   return (
-    <form className="p-64" onSubmit={handleSubmit}>
-      <label htmlFor="description">Description</label>
-      <input
-        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block  appearance-none leading-normal"
-        type="text"
-        name="description"
-        id="description"
-        onChange={handleBillChange}
-      />
-      <label htmlFor="totalAmount">Total Amount</label>
-      <input
-        className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block  appearance-none leading-normal"
-        type="number"
-        name="totalAmount"
-        id="totalAmount"
-        onChange={handleBillChange}
-      />
-      <input
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        type="button"
-        value="Add User"
-        onClick={addUser}
-      />
+    <userContext.Consumer>
+      {({user}) => {
+        setUserIdState(user.id);
 
-      {userState.map((val, idx) => {
-        const userId = `name-${idx}`;
-        const amountId = `amount-${idx}`;
         return (
-          <div key={`user-${idx}`}>
-            <label htmlFor={userId}>{`User #${idx + 1}`}</label>
+          <form className="p-64" onSubmit={handleSubmit}>
+            <label htmlFor="description">Description</label>
             <input
               className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block  appearance-none leading-normal"
-              data-type="name"
               type="text"
-              name={userId}
-              data-idx={idx}
-              id={userId}
-              value={userState[idx].name}
-              onChange={handleUserChange}
+              name="description"
+              id="description"
+              onChange={handleBillChange}
             />
-            <label htmlFor={amountId}>Amount</label>
+            <label htmlFor="totalAmount">Total Amount</label>
             <input
               className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block  appearance-none leading-normal"
-              data-type="amount"
               type="number"
-              name={amountId}
-              data-idx={idx}
-              id={amountId}
-              value={userState[idx].amount}
-              onChange={handleUserChange}
+              name="totalAmount"
+              id="totalAmount"
+              onChange={handleBillChange}
             />
-          </div>
-        );
-      })}
+            <input
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="button"
+              value="Add User"
+              onClick={addUser}
+            />
 
-      <input
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        type="submit"
-        value="Submit"
-      />
-    </form>
+            {userState.map((val, idx) => {
+              const userId = `name-${idx}`;
+              const amountId = `amount-${idx}`;
+              return (
+                <div key={`user-${idx}`}>
+                  <label htmlFor={userId}>{`User #${idx + 1}`}</label>
+                  <input
+                    className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block  appearance-none leading-normal"
+                    data-type="name"
+                    type="text"
+                    name={userId}
+                    data-idx={idx}
+                    id={userId}
+                    value={userState[idx].name}
+                    onChange={handleUserChange}
+                  />
+                  <label htmlFor={amountId}>Amount</label>
+                  <input
+                    className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block  appearance-none leading-normal"
+                    data-type="amount"
+                    type="number"
+                    name={amountId}
+                    data-idx={idx}
+                    id={amountId}
+                    value={userState[idx].amount}
+                    onChange={handleUserChange}
+                  />
+                </div>
+              );
+            })}
+
+            <input
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+              value="Submit"
+            />
+          </form>
+        );
+      }}
+    </userContext.Consumer>
   );
 };
 
